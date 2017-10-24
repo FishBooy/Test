98c8bf74c0619e6fd9966b8ebf4424a7a2cf0cf1
kevin <qwk.love@gmail.com>
2017-10-24 23:47:47 +0800

    first Commit
    first
    local to github

---
+++ js/test.js
@@ -0,0 +1,21 @@
+
+var timeId;
+$(function(){
+	//hove效果
+	$('.article-wrap')
+	.mouseover(function(){
+		$(this).addClass('chosen-arc').prev().addClass('chosen-prev');
+	})
+	.mouseout(function(){
+		$(this).removeClass('chosen-arc').prev().removeClass('chosen-prev');
+	});
+
+	// $(window).resize(function(){
+	// 	timeId && clearTimeout(timeId);
+	// 	timeId=setTimeout(function(){
+	// 		alert('width:'+$(window).width()+'--height:'+$(window).height()+'--h2:'+$('h2').css('fontSize')+'--p!:'+$('p').css('fontSize'));
+	// 	},100);
+		
+	// });
+	// $(window).trigger('resize')
+})
